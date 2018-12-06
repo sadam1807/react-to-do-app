91e9f649c2b0ea391851093ceffeaddcba06be2a
sadam <sanjeet.kanekar@sjinnovation.com>
2018-12-06 22:45:53 +0530

    listing and deleting of todo state

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Todos from './Todos';
 
 class App extends Component {
+  state = {
+    todos: [
+      {id:1, content: 'buy some milk'},
+      {id:2, content: 'play Mario Kart'}
+    ]
+  }
+
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    })
+    this.setState ({
+      todos
+    })
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="todo-app container">
+         <h1 className="center blue-text">Todo's</h1>
+         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
       </div>
     );
   }
